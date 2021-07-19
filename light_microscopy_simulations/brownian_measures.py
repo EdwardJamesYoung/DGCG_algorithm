a1cb385cd72851fcb177adbdbed91b9770fab7df
EdwardJamesYoung <edwardjamesyoung3@gmail.com>
2021-07-19 16:27:23 +0100

    Added get_noisy_data function

--- light_microscopy_simulations/brownian_measures.py
+++ light_microscopy_simulations/brownian_measures.py
@@ -19,11 +19,15 @@ def generate_brownian_curve(T,S,centre = np.array([0.5,0.5])):
 
 def generate_brownian_measure(T,scalings,centres,intensities):
     """
-    Inputs:
+    Parameters:
     T - int - number of time steps over which the motion is to be simulated
     scalings - np.ndarray of shape (n,1) - the scale of the each of the motions
     centres - np.ndarray of shape (n,2) - the startin point of each of the motions
     intensities - np.ndarray of shape (2,1) - the intensities of each of the motions
+
+    Returns:
+    A classes.measure object created by adding together n Brownian motions, with the centres,
+        scalings, and intensities as given by the parameters.
     """
     brownian_measure = DGCG.classes.measure()
     n = scalings.shape[0]
@@ -31,3 +35,23 @@ def generate_brownian_measure(T,scalings,centres,intensities):
         brownian_curve = DGCG.classes.curve(generate_brownian_curve(T,scalings[ii],centre=centres[ii,:]))
         brownian_measure.add(brownian_curve,intensities[ii]*brownian_curve.energy())
     return brownian_measure
+
+def get_noisy_data(measure,noise_level):
+    """
+    Parameters: 
+    noise_level - float - This is the reciprocal of the s/n ratio, or equivalently the ratio of the noise vector to the signal vector
+        measure - The measure from which the data is to be generated.
+
+    Returns:
+    numpy.ndarray of shape equal to that of DGCG.operators.K_t_star_full(measure) - This is the noisy data
+    """    
+    #Generate data from the measure
+    data = DGCG.operators.K_t_star_full(measure)
+    #Generate random noise vector using a Gaussian distribution
+    noise = np.random.randn(*data.shape)
+    #Normalise the noise vector
+    noise = noise/np.sqrt(DGCG.operators.int_time_H_t_product(noise,noise))
+    #Scale the noise vector back up
+    noise = noise*noise_level*np.sqrt(DGCG.operators.int_time_H_t_product(data,data))
+
+    return data + noise
